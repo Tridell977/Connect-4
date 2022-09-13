88be25119a598333c54fc6921f1a2390207c4f80
Justin <Tridell977@gmail.com>
2022-09-12 22:33:04 -0600

    added players

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const boardElem = document.getElementById('game_board')
 const game = [];
 const playerOne = "Red";
 const playerTwo = "Yellow";
+let nameCounter = 0;
+const submitButton = document.getElementById('nameSubmit');
+const nameEnter = document.getElementById('enterName');
 let currentPlayer = playerOne;
 function buildinitialGame() {
      for(let i = 0; i < 7; i++){
@@ -38,4 +41,17 @@ function colorChange(event){
     }
 
 }
-console.log(columnElem);
+submitButton.onclick = function(){
+    const one = document.getElementById('oneName');
+    const two = document.getElementById('twoName');
+    if(nameCounter === 0){
+    one.innerText = nameEnter.value;
+    nameEnter.value = "";
+    nameEnter.placeholder = "Player Two Name";
+    nameCounter++;
+    } else {
+        two.innerText = nameEnter.value;
+        nameEnter.value = "Enjoy the Game!"
+        nameEnter.disabled = true;
+    }
+}
