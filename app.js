997c4b46e652c1fa4d119e2e6f900fdabcede2aa
Justin <Tridell977@gmail.com>
2022-09-15 10:16:09 -0600

    drop chip added"

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const game = [];
 const playerOne = "Red";
 const playerTwo = "Yellow";
 let nameCounter = 0;
+let dropArray = [5, 5, 5, 5, 5, 5, 5]
 const submitButton = document.getElementById('nameSubmit');
 const nameEnter = document.getElementById('enterName');
 let currentPlayer = playerOne;
@@ -16,9 +17,11 @@ function renderGame() {
     for(let i = 0; i < 7; i++){
         columnElem = document.createElement('div');
         boardElem.appendChild(columnElem);
+        columnElem.classList.add('column')
         for(let j = 0; j < 6; j++){
         const circleElem = document.createElement('div');
         circleElem.classList.add('circle');
+        circleElem.id = i + ',' + j;
         columnElem.appendChild(circleElem);
         }
     }
@@ -31,13 +34,18 @@ boardElem.addEventListener('click', colorChange)
 
 function colorChange(event){
     if(event.target.className === 'circle'){
+        const circleIndex = event.target.id.split(',');
+        const x = circleIndex[0];
+        let y = dropArray[x];
+        dropCircle = document.getElementById(`${x},${y}`);
         if(currentPlayer === playerOne){
-        event.target.classList.add('red');
+        dropCircle.classList.add('red');
         currentPlayer = playerTwo;
         } else {
-            event.target.classList.add('yellow');  
+            dropCircle.classList.add('yellow'); 
             currentPlayer = playerOne;
         }
+        dropArray[x]--;
     }
 
 }
